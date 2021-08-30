0d2eeac04f03a6df057f9a8c5f04ff94d7ecdb70
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-08-30 14:47:49 +0100

    Game Finished

---
+++ NumberGuessingApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NumberGuessingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0 and 100: ");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+
+                if( i > winNum)
+                {
+                    Console.Write("Too high! try again");
+                }
+                else if (i < winNum)
+                {
+                    Console.Write("Too low! try again");
+
+                }
+                else if (i == winNum)
+                {
+                    Console.Write("You WIN!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing");
+            Console.Write("Press any key to finish");
+            Console.ReadKey(true);
+        }
+    }
+}
